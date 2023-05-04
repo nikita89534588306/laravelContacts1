e48b56389fd54814b937a642d955c3a7972974fe
Nikita Balashov <noWorkingEmail@mail.ru>
2023-05-04 10:51:52 +0500

    написан скрипт для маскирования телефонного номера

--- resources/views/contact/create.blade.php
+++ resources/views/contact/create.blade.php
@@ -27,7 +27,7 @@
                     <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"	 with='32.00' height='32.00' viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" xml:space="preserve"><g>	<path d="M256,32c123.5,0,224,100.5,224,224S379.5,480,256,480S32,379.5,32,256S132.5,32,256,32 M256,0C114.625,0,0,114.625,0,256		s114.625,256,256,256s256-114.625,256-256S397.375,0,256,0L256,0z M398.719,341.594l-1.438-4.375		c-3.375-10.062-14.5-20.562-24.75-23.375L334.688,303.5c-10.25-2.781-24.875,0.969-32.405,8.5l-13.688,13.688		c-49.75-13.469-88.781-52.5-102.219-102.25l13.688-13.688c7.5-7.5,11.25-22.125,8.469-32.406L198.219,139.5		c-2.781-10.25-13.344-21.375-23.406-24.75l-4.313-1.438c-10.094-3.375-24.5,0.031-32,7.563l-20.5,20.5		c-3.656,3.625-6,14.031-6,14.063c-0.688,65.063,24.813,127.719,70.813,173.75c45.875,45.875,108.313,71.345,173.156,70.781		c0.344,0,11.063-2.281,14.719-5.938l20.5-20.5C398.688,366.062,402.062,351.656,398.719,341.594z"/></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g></svg>
                 </div>
                 <div class="col-11 phones_input">
-                    <input type="phone" name="phone_1" id='phone_1' class="form-control mb-3" placeholder="Номер телефона">
+                    <input type="phone" name="phone_1" id='phone_1' class="form-control mb-3 phone_input" placeholder="Номер телефона">
                 </div>
             </div>
             <button type="submit" class="btn btn-primary">Submit</button>
@@ -36,31 +36,60 @@
 @endsection
 
 @section('sctipts')
+<script src="/js/test.js"></script>
 <script>
-
-
     var phonesInput = document.querySelector('.phones_input');
     var phoneInput = phonesInput.lastElementChild;
-    function createInput(){
-        
-        this.removeEventListener('input', createInput);
 
-        newInputPhone = document.createElement('input')
-        newInputPhone.placeholder = 'Номер телефона';
-        newInputPhone.type = 'phone';
-        newInputPhone.name = 'phone_id' + Date.now();
-        newInputPhone.classList.add("form-control");
-        newInputPhone.classList.add("mb-3");
+    var maskForPhoneNumber = [
+            {  mask: '0 (000) 000-00-00', startsWith: '8' },
+            {  mask: '+0 (000) 000-00-00', startsWith: '7' },
+            {  mask: '*0 (000) 000-00-00', startsWith: '+' },
+            {  mask: '(000) 000-00-00', startsWith: '' },
+    ]
+    function funcSelectMask(appended, dynamicMasked) {
+        var number = (dynamicMasked.value + appended).replace(/\s/g,'');
 
-        phonesInput.appendChild(newInputPhone);
-        
-        newInputPhone.addEventListener('input', createInput)
-        newInputPhone.addEventListener('input', deleteInput)
-        
+        return dynamicMasked.compiledMasks.find(function (m) {
+            return number.indexOf(m.startsWith) === 0;
+        });
     }
-    function deleteInput(){ if(this.value === '') this.remove();}
+    phonesInput.mask = IMask(phoneInput, {
+        mask: maskForPhoneNumber,
+        dispatch: funcSelectMask
+    })
+
+
+    function createInput(){
+        if(this.value !== ''){
+            this.removeEventListener('input', createInput);
+
+            var inputId = 'phone_id' + Date.now();
+
+            newInputPhone = document.createElement('input')
+            newInputPhone.name = inputId;
+            newInputPhone.placeholder = 'Номер телефона';
+            newInputPhone.type = 'phone';
+            newInputPhone.classList.add("form-control");
+            newInputPhone.classList.add("mb-3");
+
+            newInputPhone.newMask = IMask(newInputPhone, {
+                mask: maskForPhoneNumber,
+                dispatch: funcSelectMask
+            })
+
+
+            
+            phonesInput.appendChild(newInputPhone);
+            
+            newInputPhone.addEventListener('input', createInput)
+            this.addEventListener('input', deleteInput)
+
+        }    
+    }
+    function deleteInput(){ if(this.value === '') { this.mask.destroy(); this.remove()};}
+
     phoneInput.addEventListener('input', createInput)
-    phoneInput.addEventListener('input', deleteInput)
 
 
 </script>
